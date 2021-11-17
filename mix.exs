5c449d9d0bd3173f6b9938a806e768a357cb023f
German Velasco <germsvel@gmail.com>
2021-11-17 05:02:36 -0500

    Update several dependencies
    We wanted to use Phoenix LiveView 0.17. While we're updating that
    dependency, and since this isn't a production system, we update as many
    as we can by unlocking all of our dependencies. We have to bump
    `phoenix_html` and `phoenix` versions to resolve conflicts, and we also
    resolve any npm vulnerabilities that could be fixed automatically.

--- mix.exs
+++ mix.exs
@@ -33,13 +33,13 @@ defmodule Gui.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
+      {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.15.0"},
+      {:phoenix_live_view, "~> 0.17.0"},
       {:floki, ">= 0.27.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
