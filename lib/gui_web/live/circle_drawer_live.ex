feb77e7a83f29957d20d5c5d291c362dbcd05519
German Velasco <germsvel@gmail.com>
2021-12-20 05:55:47 -0500

    Add canvas

--- lib/gui_web/live/circle_drawer_live.ex
+++ lib/gui_web/live/circle_drawer_live.ex
@@ -4,6 +4,15 @@ defmodule GuiWeb.CircleDrawerLive do
   def render(assigns) do
     ~H"""
     <h1>CircleDraw</h1>
+
+    <canvas class="border-2 border-gray-800" id="circle-drawer" width="300" height="300">
+      Circle Drawer Canvas
+    </canvas>
+
+    <script>
+      let canvas = document.getElementById('circle-drawer');
+      let ctx = canvas.getContext('2d');
+    </script>
     """
   end
 end
