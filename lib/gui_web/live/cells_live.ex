dc6a8142d48e2f240bf5fc1612b4a386d9605e9d
German Velasco <germsvel@gmail.com>
2022-08-04 08:24:15 -0400

    Generate rows and cols

--- lib/gui_web/live/cells_live.ex
+++ lib/gui_web/live/cells_live.ex
@@ -4,6 +4,35 @@ defmodule GuiWeb.CellsLive do
   def render(assigns) do
     ~H"""
     <h1 class="font-semibold">Cells</h1>
+
+    <table>
+      <tbody>
+        <tr>
+          <td></td>
+          <%= for col_title <- @col_titles do %>
+            <th scope="col"><%= col_title %></th>
+          <% end %>
+        </tr>
+
+        <%= for row_title <- @row_titles do %>
+          <tr>
+            <th scope="row"><%= row_title %></th>
+          </tr>
+        <% end %>
+      </tbody>
+    </table>
     """
   end
+
+  @col_titles ~w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+  @row_titles for i <- 0..99, do: to_string(i)
+
+  def mount(_, _, socket) do
+    socket
+    |> assign(:col_titles, @col_titles)
+    |> assign(:row_titles, @row_titles)
+    |> ok()
+  end
+
+  defp ok(socket), do: {:ok, socket}
 end
--- test/gui_web/live/cells_live_test.exs
+++ test/gui_web/live/cells_live_test.exs
@@ -8,4 +8,18 @@ defmodule GuiWeb.CellsLiveTest do
 
     assert html =~ "Cells"
   end
+
+  test "renders columns [A-Z] and rows [0-99]", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/cells")
+
+    assert has_element?(view, col_title(), "A")
+    assert has_element?(view, col_title(), "G")
+    assert has_element?(view, col_title(), "Z")
+    assert has_element?(view, row_title(), "0")
+    assert has_element?(view, row_title(), "50")
+    assert has_element?(view, row_title(), "99")
+  end
+
+  defp col_title, do: "th[scope=col]"
+  defp row_title, do: "th[scope=row]"
 end
