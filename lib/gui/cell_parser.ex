af5ac8b2f01b9e3cb5ec1398362693a04959ed6c
German Velasco <germsvel@gmail.com>
2022-08-08 20:50:38 -0400

    Tag all resulst

--- lib/gui/cell_parser.ex
+++ lib/gui/cell_parser.ex
@@ -1,7 +1,9 @@
 defmodule Gui.CellParser do
   import NimbleParsec
 
-  identifier = repeat(ascii_string([?a..?z, ?A..?Z], min: 1))
+  identifier =
+    repeat(ascii_string([?a..?z, ?A..?Z], min: 1))
+    |> tag(:iden)
 
   number =
     optional(string("-"))
@@ -9,15 +11,18 @@ defmodule Gui.CellParser do
     |> optional(string("."))
     |> optional(integer(min: 1))
     |> eos()
+    |> tag(:number)
 
   coord =
     ascii_string([?a..?z, ?A..?Z], 1)
     |> integer(min: 1, max: 2)
+    |> tag(:coord)
 
   range =
     coord
     |> ignore(string(":"))
     |> concat(coord)
+    |> tag(:range)
 
   defparsec(:coord, coord)
   defparsec(:range, range)
--- test/gui/cell_parser_test.exs
+++ test/gui/cell_parser_test.exs
@@ -4,24 +4,26 @@ defmodule Gui.CellParserTest do
   alias Gui.CellParser
 
   test "parses a coordinate" do
-    {:ok, ["A", 0], "", %{}, {1, 0}, 2} = CellParser.coord("A0")
-    {:ok, ["Z", 99], "", %{}, {1, 0}, 3} = CellParser.coord("Z99")
+    {:ok, [coord: ["A", 0]], "", %{}, {1, 0}, 2} = CellParser.coord("A0")
+    {:ok, [coord: ["Z", 99]], "", %{}, {1, 0}, 3} = CellParser.coord("Z99")
   end
 
   test "parses a range" do
-    {:ok, ["A", 0, "A", 9], "", _, _, _} = CellParser.range("A0:A9")
-    {:ok, ["A", 0, "Z", 0], "", _, _, _} = CellParser.range("A0:Z0")
-    {:ok, ["F", 10, "Z", 99], "", _, _, _} = CellParser.range("F10:Z99")
+    {:ok, [range: [coord: ["A", 0], coord: ["A", 9]]], "", _, _, _} = CellParser.range("A0:A9")
+    {:ok, [range: [coord: ["A", 0], coord: ["Z", 0]]], "", _, _, _} = CellParser.range("A0:Z0")
+
+    {:ok, [range: [coord: ["F", 10], coord: ["Z", 99]]], "", _, _, _} =
+      CellParser.range("F10:Z99")
   end
 
   test "parses numbers" do
-    {:ok, [29], "", _, _, _} = CellParser.number("29")
-    {:ok, [23, ".", 9], "", _, _, _} = CellParser.number("23.9")
-    {:ok, ["-", 0, ".", 333], "", _, _, _} = CellParser.number("-0.333")
+    {:ok, [number: [29]], "", _, _, _} = CellParser.number("29")
+    {:ok, [number: [23, ".", 9]], "", _, _, _} = CellParser.number("23.9")
+    {:ok, [number: ["-", 0, ".", 333]], "", _, _, _} = CellParser.number("-0.333")
   end
 
   test "parses identifiers" do
-    {:ok, ["sum"], "", _, _, _} = CellParser.identifier("sum")
-    {:ok, ["div"], "", _, _, _} = CellParser.identifier("div")
+    {:ok, [iden: ["sum"]], "", _, _, _} = CellParser.identifier("sum")
+    {:ok, [iden: ["div"]], "", _, _, _} = CellParser.identifier("div")
   end
 end
